12a0f19f655747883724fa2b14f713d6ff53d860
Steamship Studios <95093456+SteamshipStudios@users.noreply.github.com>
2021-11-26 07:40:57 -0800

    Add files via upload

---
+++ Console Alchemy 0.01.py
@@ -0,0 +1,92 @@
+#Intro
+sinsterslackerstudios = input('''
+   _____   _                                      _       _              _____   _                 _   _                   _____                                       _                     
+  / ____| | |                                    | |     (_)            / ____| | |               | | (_)                 |  __ \                                     | |                    
+ | (___   | |_    ___    __ _   _ __ ___    ___  | |__    _   _ __     | (___   | |_   _   _    __| |  _    ___    ___    | |__) |  _ __    ___   ___    ___   _ __   | |_   ___             
+  \___ \  | __|  / _ \  / _` | | '_ ` _ \  / __| | '_ \  | | | '_ \     \___ \  | __| | | | |  / _` | | |  / _ \  / __|   |  ___/  | '__|  / _ \ / __|  / _ \ | '_ \  | __| / __|            
+  ____) | | |_  |  __/ | (_| | | | | | | | \__ \ | | | | | | | |_) |    ____) | | |_  | |_| | | (_| | | | | (_) | \__ \   | |      | |    |  __/ \__ \ |  __/ | | | | | |_  \__ \  _   _   _ 
+ |_____/   \__|  \___|  \__,_| |_| |_| |_| |___/ |_| |_| |_| | .__/    |_____/   \__|  \__,_|  \__,_| |_|  \___/  |___/   |_|      |_|     \___| |___/  \___| |_| |_|  \__| |___/ (_) (_) (_)
+                                                             | |                                                                                                                             
+                                                             |_|'''                                                                       
+consolealchemy = input('''
+   _____                                 _                       _          _                                  _ 
+  / ____|                               | |              /\     | |        | |                                | |
+ | |        ___    _ __    ___    ___   | |   ___       /  \    | |   ___  | |__     ___   _ __ ___    _   _  | |
+ | |       / _ \  | '_ \  / __|  / _ \  | |  / _ \     / /\ \   | |  / __| | '_ \   / _ \ | '_ ` _ \  | | | | | |
+ | |____  | (_) | | | | | \__ \ | (_) | | | |  __/    / ____ \  | | | (__  | | | | |  __/ | | | | | | | |_| | |_|
+  \_____|  \___/  |_| |_| |___/  \___/  |_|  \___|   /_/    \_\ |_|  \___| |_| |_|  \___| |_| |_| |_|  \__, | (_)
+                                                                                                        __/ |    
+                                                                                                       |___/     '''                                                                            
+start = input('Press any button to start!')
+#Gives the player a list of basic elements to make it easier for them to play.
+print('Here is a list of elements you can combine!')
+print('     air')
+print('     earth')
+print('     fire')
+print('     water')
+
+
+
+name = input('\nWhat is your name?')
+
+def combo
+    #Asks the player what elements they want to combine, this is essential to gameplay, do not modify.
+    element1 = input(f'\nWell {name.title()}, what is the first element you want to combine?')
+    element2 = input('And what is the second element you want to combine?')
+
+    #The recipes for elements, which makes the above section actually mean something.
+    if element1.lower() == 'water':
+        if element2.lower() == 'water':
+            print(f'Woah {name.title()}! You made the sea!')
+            print('The little brother of the ocean!')
+    
+    if element1.lower() == 'sea':
+        if element2.lower() == 'sea':
+            print(f'Woah {name.title()}! You made the ocean!')
+            print('The bigger brother of the sea!')
+    
+    if element1.lower() == 'fire':
+        if element2.lower() == 'fire':
+            print(f'Woah {name.title()}! You made energy!')
+            print('E=mc squared!')
+    
+    if element1.lower() == 'energy':
+        if element2.lower() == 'air':
+            print(f'Woah {name.title()}! You made electricity!')
+            print('An electrifying discovery!')
+
+    if element1.lower() == 'electricity':
+        if element2.lower() == 'sea':
+            print(f'Woah {name.title()}! You made primordial soup!')
+            print('Sure does not taste like soup though.')
+    
+    if element1.lower() == 'primordial soup':
+        if element2.lower() == 'energy':
+            print(f'Woah {name.title()}! You made life!')
+            print('The more lively brother of death!')
+    
+    if element1.lower() == 'life':
+        if element2.lower() == 'earth':
+            print(f'Woah {name.title()}! You made animals!')
+            print('Just like animals, animals.')
+    
+    if element1.lower() or element2.lower() == 'sin':
+        print('
+def credit
+    print('All of the code: Cohen Scheffrin')
+    print('Some combination ideas: Richard Scheffrin')
+
+def mainmenu
+    print('What would you like to do?')
+    print('Play')
+    print('Credits')
+    optionalism = input('Choose! 1 or 2?')
+
+    if optionalism == '1':
+        combo()
+    if optionalism == '2':
+        credit()
+
+mainmenu()
+
+combo()
